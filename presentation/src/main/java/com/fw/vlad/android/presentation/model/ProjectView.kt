67423b1591fc41588897b6e39a423450ff4002fb
Vlad <vladcipariu@gmail.com>
2018-09-26 22:46:29 +0100

    Creating the data models for the presentation layer

---
+++ presentation/src/main/java/com/fw/vlad/android/presentation/model/ProjectView.kt
@@ -0,0 +1,10 @@
+package com.fw.vlad.android.presentation.model
+
+class ProjectView(val id: String,
+                  val name: String,
+                  val fullName: String,
+                  val starCount: String,
+                  val dateCreate: String,
+                  val ownerName: String,
+                  val ownerAvatar: String,
+                  val isBookmarked: Boolean)
