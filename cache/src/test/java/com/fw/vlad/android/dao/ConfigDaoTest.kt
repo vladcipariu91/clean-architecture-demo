bba0ce52fe8ab7e0537fc8e0b9ebdc21426e7dab
Vlad <vladcipariu@gmail.com>
2018-09-26 20:56:05 +0100

    Testing the configuration DAO

---
+++ cache/src/test/java/com/fw/vlad/android/dao/ConfigDaoTest.kt
@@ -0,0 +1,39 @@
+package com.fw.vlad.android.dao
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import com.fw.vlad.android.cache.db.ProjectsDatabase
+import com.fw.vlad.android.test.factory.ConfigDataFactory
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+
+@RunWith(RobolectricTestRunner::class)
+class ConfigDaoTest {
+
+    @Rule
+    @JvmField var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val database = Room.inMemoryDatabaseBuilder(
+            RuntimeEnvironment.application.applicationContext,
+            ProjectsDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+
+    @After
+    fun clearDb() {
+        database.close()
+    }
+
+    @Test
+    fun saveConfigurationSavesData() {
+        val config = ConfigDataFactory.makeCachedConfig()
+        database.configDao().insertConfig(config)
+
+        val testObserver = database.configDao().getConfig().test()
+        testObserver.assertValue(config)
+    }
+}
