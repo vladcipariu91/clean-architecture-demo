17e7af4635ecc208bdebda9670d19ba31eff3945
Vlad <vladcipariu@gmail.com>
2018-09-26 07:26:18 +0100

    Creating the cached project Data Access Object

--- cache/src/main/java/com/fw/vlad/android/cache/dao/CachedProjectDao.kt
+++ cache/src/main/java/com/fw/vlad/android/cache/dao/CachedProjectDao.kt
@@ -1,4 +1,32 @@
 package com.fw.vlad.android.cache.dao
 
-class CachedProjectDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.fw.vlad.android.cache.db.ProjectConstants.DELETE_PROJECTS
+import com.fw.vlad.android.cache.db.ProjectConstants.QUERY_BOOKMARKED_PROJECTS
+import com.fw.vlad.android.cache.db.ProjectConstants.QUERY_PROJECTS
+import com.fw.vlad.android.cache.db.ProjectConstants.QUERY_UPDATE_BOOKMARK_STATUS
+import com.fw.vlad.android.cache.model.CachedProject
+import io.reactivex.Flowable
+
+@Dao
+abstract class CachedProjectDao {
+
+    @Query(QUERY_PROJECTS)
+    abstract fun getProjects(): Flowable<List<CachedProject>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    abstract fun insertProjects(projects: List<CachedProject>)
+
+    @Query(DELETE_PROJECTS)
+    abstract fun deleteProjects()
+
+    @Query(QUERY_BOOKMARKED_PROJECTS)
+    abstract fun getBookmarkerProjects(): Flowable<List<CachedProject>>
+
+    @Query(QUERY_UPDATE_BOOKMARK_STATUS)
+    abstract fun setBookmarkedStatus(isBookmarked: Boolean,
+                                     projectId: String)
 }
--- cache/src/main/java/com/fw/vlad/android/cache/model/CachedProject.kt
+++ cache/src/main/java/com/fw/vlad/android/cache/model/CachedProject.kt
@@ -1,15 +1,20 @@
 package com.fw.vlad.android.cache.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.fw.vlad.android.cache.db.ProjectConstants
 
 @Entity(tableName = ProjectConstants.TABLE_NAME)
-data class CachedProject(@PrimaryKey var id: String,
-                         var name: String,
-                         var fullName: String,
-                         var starCount: String,
-                         var dateCreated: String,
-                         var ownerName: String,
-                         var ownerAvatar: String,
-                         var isBookmarked: Boolean)
+data class CachedProject(
+        @PrimaryKey
+        @ColumnInfo(name = ProjectConstants.COLUMN_PROJECT_ID)
+        var id: String,
+        var name: String,
+        var fullName: String,
+        var starCount: String,
+        var dateCreated: String,
+        var ownerName: String,
+        var ownerAvatar: String,
+        @ColumnInfo(name = ProjectConstants.COLUMN_IS_BOOKMARKED)
+        var isBookmarked: Boolean)
